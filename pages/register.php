17afa50a3c3025856b1c7b719df08db8c3f0ae75
Dima Gimazetdinov <dmitrycube7@gmail.com>
2021-04-13 23:26:36 +0300

    write start files

---
+++ pages/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+
+    <link rel="stylesheet" href="./../css/register.css">
+    <link rel="stylesheet" href="./../css/tags.css">
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@200;300;400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <form action="" method="get">
+            <label for="name">Name:</label>
+            <input type="text" name="name"></br>
+            <label for="sname">Surname:</label>
+            <input type="text" name="sname"></br>
+            <div class="pass">
+                <label for="pass">Password:</label>
+                <input type="text" name="pass"> <span class="eye">
+            </span></div>
+            <label for="rpass">Repet password:</label>
+            <input type="text" name="rpass"></br>
+
+            <input type="submit" class="btn" name="btn" value="Create acount">
+        </form>
+    </main>
+</body>
+</html>
